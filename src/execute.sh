bb7692fa790fb49ed0672d9ef9e87d0bc060e5af
ForkΨKILLET <fork_killet@qq.com>
2022-10-09 17:20:58 +0800

    upd: `execute` now receive cmds from args instead of $CMD

--- src/execute.sh
+++ src/execute.sh
@@ -3,16 +3,17 @@
 source ${0:A:h}/utils/color.sh
 
 DEV="${DEV:-./devices}"
-CMD="${CMD:-shell echo hello}"
 LOG="${LOG:-./log}"
 
-echo "[@: $(date +%Y/%m/%d-%H:%M:%S)] \$ adb $CMD" >> $LOG
+cmd=${@:-shell echo hello}
+
+echo "[@: $(date +%Y/%m/%d-%H:%M:%S)] \$ adb $cmd" >> $LOG
 
 for ip in $(<$DEV); do
-	echo $(green running) \$ adb -s $ip $CMD
+	echo $(green running) \$ adb -s $ip $cmd
 	[[ -n "$BG" ]] && and='&'
 	sed="0,/^/s//[ip: $ip] /;\$a\\"
-	eval "adb -s $ip $CMD 2>&1 | sed -e '$sed' >> $LOG $and"
+	eval "adb -s $ip $cmd 2>&1 | sed -e '$sed' >> $LOG $and"
 done
 
 read -k 1
