9845fefdeb62e3a3ba283d4de50408fcab8a24af
ForkΨKILLET <fork_killet@qq.com>
2022-10-13 20:27:18 +0800

    feat(`scan`): enable to scan IP by pattern

--- src/scan.sh
+++ src/scan.sh
@@ -2,13 +2,35 @@
 
 source ${0:A:h}/utils/color.sh
 
-for i in {${START:-0}..${END:-255}}; do
-	ip=10.76.${IP3:-23}.$i:5555
+ippat=(${(s[.])1})
+port=${2:-5555}
+
+try-ip () {
+	local ip=$1
 	echo $(green trying) $ip
 	{
 		out=$(adb connect $ip 2>&1)
 		[[ $out =~ 'fail*' ]] || echo $(blue succeeded) $ip
 	} &
+}
+
+exp=()
+
+for pat in $ippat; do
+	if [[ $pat = '-' ]]; then
+		exp+="{0..255}"
+	elif [[ $pat =~ '^[0-9]+-[0-9]+$' ]]; then
+		range=(${(s[-])pat})
+		exp+="{${range[1]}..${range[2]}}"
+	elif [[ $pat =~ '^[0-9]+$' ]]; then
+		exp+="$pat"
+	else
+		echo $(red illegal pattern: $pat)
+		exit 1
+	fi
 done
+
+ipe="${(j[.])exp}:$port"
+eval "for ip in $ipe; do try-ip \$ip; done"
 
 read -k 1
