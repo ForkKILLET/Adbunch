bc01c067b3be08b2a88378c621a623ce6526eca6
ForkΨKILLET <fork_killet@qq.com>
2022-10-13 17:38:19 +0800

    fix(`nicereboot`).

--- src/nicereboot.sh
+++ src/nicereboot.sh
@@ -2,7 +2,7 @@
 
 source ${0:A:h}/utils/color.sh
 
-BG=1 CMD="shell reboot" ${0:A:h}/execute.sh
+BG=1 ${0:A:h}/execute.sh shell reboot
 
 echo $(green reloading devs)
 ${0:A:h}/loaddevs.sh > /dev/null
